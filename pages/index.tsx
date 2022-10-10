9e4c3516db96a2f3932d59d9b2a0c0c0f20ba837
ical10 <mochhusnir@gmail.com>
2022-10-10 23:50:12 +0700

    feat: adjusted text

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,9 +13,8 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Welcome to Perma-Tweeter!</h1>
+        <button>Login with Twitter here</button>
       </main>
 
       <footer className={styles.footer}>
