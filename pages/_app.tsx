bfd9d22b7f03257bbfc57195a9448e3022f7119a
ical10 <mochhusnir@gmail.com>
2022-10-11 19:12:53 +0700

    feat: configured nextauth session

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps<{ session: Session }>) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
 export default MyApp;
