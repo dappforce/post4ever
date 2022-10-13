a63feb4641f48c23930e6ec05ca49822133d52a3
ical10 <mochhusnir@gmail.com>
2022-10-13 15:05:36 +0700

    feat: adjusted style

--- pages/tweets.tsx
+++ pages/tweets.tsx
@@ -29,7 +29,12 @@ const TweetPage: NextPage<TweetsProps> = ({ tweets }) => {
     return (
       <div>
         <p>Access unauthorized, please login first</p>
-        <button onClick={() => router.push("/")}>Go back to login</button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => router.push("/")}
+        >
+          Go back to login
+        </button>
       </div>
     );
 
@@ -67,20 +72,23 @@ const TweetPage: NextPage<TweetsProps> = ({ tweets }) => {
       </Head>
 
       <div className="flex flex-row items-center justify-center max-w-full max-h-screen">
-        <div>
+        <div className="flex flex-col self-start mt-4 p-4 gap-2 max-w-[500px]">
           <AuthButton text={"Logout"} />
+          <a>{`Welcome! You are logged in as @${session.user.name}`}</a>
         </div>
         <div className="flex flex-row max-h-screen p-4">
           <div className="flex flex-col overflow-y-auto max-w-[640px]">
             {tweets.map((tweet) => (
               <div
                 key={tweet.id}
-                className="flex flex-col min-w-full items-center p-4 mb-4 border-2 border-white rounded"
+                className="p-6 max-w-lg bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-blue-500 dark:hover:bg-blue-500
+                flex flex-col items-center mb-4"
               >
                 <div className="flex flex-row items-center self-start justify-center gap-2">
                   <Image
                     src={session.user?.image ?? ""}
                     alt="user-avatar"
+                    className="rounded-full"
                     width="32"
                     height="32"
                   />
@@ -105,16 +113,23 @@ const TweetPage: NextPage<TweetsProps> = ({ tweets }) => {
             ))}
           </div>
         </div>
-        <div>
+        <div className="flex flex-col self-start items-center justify-center mt-4 p-4 gap-2 max-w-[500px]">
           <button
+            className="bg-blue-500 disabled:bg-gray-300 disabled:hover:bg-gray-100 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             onClick={handlePostTransaction}
             disabled={
-              !(savedPosts.length > 0 || savedPosts.length <= 2) ? true : false
+              savedPosts.length === 0 || savedPosts.length > 2 ? true : false
             }
           >
-            Send to IPFS!
+            {`Send ${
+              savedPosts.length === 0 ? "" : savedPosts.length
+            } post(s) to IPFS!`}
           </button>
-          <div>This is wallet section</div>
+          <a>
+            {savedPosts.length > 2
+              ? "Max 2 posts to be saved!"
+              : "Select tweets to be saved"}
+          </a>
         </div>
       </div>
     </>
