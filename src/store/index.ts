a17307bf04dfaa1326b2e44bc73b37ea374e67bc
ical10 <mochhusnir@gmail.com>
2022-11-30 23:10:41 +0700

    feat: add new store for twitter user

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,43 @@
 import create from "zustand";
 import { InjectedAccountWithMeta } from "@polkadot/extension-inject/types";
+import produce from "immer";
 
 interface WalletState {
   account: InjectedAccountWithMeta | null;
   setAccount: (newAccount: InjectedAccountWithMeta) => void;
 }
 
-export const useWalletStore = create<WalletState>()((set) => ({
+interface TwitterUserState {
+  user: TwitterUserProps;
+  setNewUser: (newUser: TwitterUserProps) => void;
+}
+
+type TwitterUserProps = {
+  id: string;
+  name: string;
+  profile_image_url: string;
+  username: string;
+};
+
+export const useWalletStore = create<WalletState>()(set => ({
   account: null,
-  setAccount: (newAccount) => set((state) => ({ account: newAccount })),
+  setAccount: newAccount => set(state => ({ account: newAccount })),
+}));
+
+export const useTwitterUserStore = create<TwitterUserState>()(set => ({
+  user: {
+    id: "",
+    name: "",
+    profile_image_url: "",
+    username: "",
+  },
+  setNewUser: (newUser: TwitterUserProps) =>
+    set(
+      produce((state: TwitterUserState) => {
+        (state.user.id = newUser.id),
+          (state.user.name = newUser.name),
+          (state.user.profile_image_url = newUser.profile_image_url),
+          (state.user.username = newUser.username);
+      }),
+    ),
 }));
