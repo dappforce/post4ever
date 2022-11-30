1422560ca3f45dbe1b27f25d0b2c6de1e31980e7
ical10 <mochhusnir@gmail.com>
2022-11-30 23:13:35 +0700

    chore: add todo

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -9,16 +9,16 @@ type AppbarProps = {
   children?: React.ReactNode;
 };
 
+//TODO: to be removed after all functionalities ported to Layout
 const Appbar = ({ children }: AppbarProps) => {
-  const { account, setAccount } = useWalletStore((state) => ({
+  const { account, setAccount } = useWalletStore(state => ({
     account: state.account,
     setAccount: state.setAccount,
   }));
 
   const [walletAuthorized, setWalletAuthorized] = useState(false);
   const [accounts, setAccounts] = useState<InjectedAccountWithMeta[]>([]);
-  const [selectedAccount, setSelectedAccount] =
-    useState<InjectedAccountWithMeta | null>(null);
+  const [selectedAccount, setSelectedAccount] = useState<InjectedAccountWithMeta | null>(null);
 
   const getAccounts = async () => {
     const extensions = await web3Enable("Perma-Tweeter dapp");
@@ -40,7 +40,7 @@ const Appbar = ({ children }: AppbarProps) => {
     const target = event.target as HTMLButtonElement;
     const address = target.value;
 
-    const found = accounts.find((account) => account.address === address);
+    const found = accounts.find(account => account.address === address);
 
     setSelectedAccount(found!);
     setAccount(found!);
@@ -49,7 +49,7 @@ const Appbar = ({ children }: AppbarProps) => {
   const trimMiddleString = (text?: string, numberStringsKept = 5) => {
     if (!text) return "";
     const temp = `${text.slice(0, numberStringsKept)}...${text.slice(
-      text.length - numberStringsKept
+      text.length - numberStringsKept,
     )}`;
 
     return temp;
@@ -58,21 +58,16 @@ const Appbar = ({ children }: AppbarProps) => {
   return (
     <div>
       <div className="flex flex-row p-2 justify-start items-center border-b-2 border-b-gray-500">
-        <div className="mr-auto text-blue-500 text-2xl font-bold">
-          Perma-Tweeter
-        </div>
-
+        <div className="mr-auto text-2xl font-bold">Perma-Tweeter</div>
         <ul className="flex">
           <li className="mr-6">
             <Link href="/tweets" legacyBehavior>
-              <a className="text-blue-500 hover:text-blue-800 text-lg font-semibold">
-                Timeline
-              </a>
+              <a className="text-primary hover:text-blue-800 text-lg font-semibold">Timeline</a>
             </Link>
           </li>
           <li className="mr-6">
             <Link href="/crosspost" legacyBehavior>
-              <a className="text-blue-500 hover:text-blue-800 text-lg font-semibold">
+              <a className="text-primary hover:text-blue-800 text-lg font-semibold">
                 Cross-post tweet
               </a>
             </Link>
@@ -83,16 +78,12 @@ const Appbar = ({ children }: AppbarProps) => {
           <div className="group inline-block relative">
             <button
               id="connect-button-with-address"
-              className="bg-orange-500 hover:bg-orange-700 text-white font-semibold py-2 px-4 rounded inline-flex items-center"
-            >
-              <span className="mr-1">
-                {trimMiddleString(selectedAccount?.address)}
-              </span>
+              className="bg-orange-500 hover:bg-orange-700 text-white font-semibold py-2 px-4 rounded inline-flex items-center">
+              <span className="mr-1">{trimMiddleString(selectedAccount?.address)}</span>
               <svg
                 className="fill-current h-4 w-4"
                 xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
+                viewBox="0 0 20 20">
                 <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />{" "}
               </svg>
             </button>
@@ -100,13 +91,11 @@ const Appbar = ({ children }: AppbarProps) => {
               {accounts.map(({ address }) => (
                 <li
                   key={address}
-                  className="first:rounded-t last:rounded-b bg-orange-500 hover:bg-orange-700"
-                >
+                  className="first:rounded-t last:rounded-b bg-orange-500 hover:bg-orange-700">
                   <button
                     className="py-2 px-4 block whitespace-no-wrap"
                     value={address}
-                    onClick={handleChangeAccount}
-                  >
+                    onClick={handleChangeAccount}>
                     {trimMiddleString(address)}
                   </button>
                 </li>
@@ -117,8 +106,7 @@ const Appbar = ({ children }: AppbarProps) => {
           <button
             id="connect-button"
             onClick={handleConnect}
-            className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"
-          >
+            className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
             Connect Polkadot.js
           </button>
         )}
