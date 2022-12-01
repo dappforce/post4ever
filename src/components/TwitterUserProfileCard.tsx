ba21a600b86abf43ba243d7467a041a70122c9dc
ical10 <mochhusnir@gmail.com>
2022-12-02 00:30:51 +0700

    feat: adjust styles and change button

--- src/components/TwitterUserProfileCard.tsx
+++ src/components/TwitterUserProfileCard.tsx
@@ -1,15 +1,18 @@
-import { Card, Avatar } from "react-daisyui";
+import { Card, Avatar, Button } from "react-daisyui";
 import { TweetUserProps } from "src/types/common";
-import AuthButton from "src/components/Button";
+
+import { signOut } from "next-auth/react";
+
+import { ArrowRightOnRectangleIcon } from "@heroicons/react/20/solid";
 
 type TwitterUserProfileCardProps = {
   authenticatedUser: TweetUserProps;
 };
 
 const TwitterUserProfileCard = ({ authenticatedUser }: TwitterUserProfileCardProps) => (
-  <Card className="shadow-2xl m-4 bg-white h-fit" bordered={false}>
-    <Card.Body className="items-start gap-4">
-      <div className="flex flex-row items-center justify-start gap-2">
+  <Card className="shadow-2xl bg-white h-fit" bordered={false}>
+    <Card.Body className="flex flex-row justify-center gap-4">
+      <div className="flex flex-row justify-center items-center gap-2">
         <Avatar src={authenticatedUser.profile_image_url} size="xs" shape="circle" />
         <a
           className="font-semibold text-base-100"
@@ -17,9 +20,19 @@ const TwitterUserProfileCard = ({ authenticatedUser }: TwitterUserProfileCardPro
           rel="noopener noreferrer"
           href={`https://twitter.com/${authenticatedUser.username}`}>{`@${authenticatedUser.username}`}</a>
       </div>
-      <Card.Actions className="w-full">
-        <AuthButton text={"Logout"} />
-      </Card.Actions>
+      <div className="ml-auto">
+        <Button
+          shape="square"
+          size="md"
+          onClick={() =>
+            signOut({
+              callbackUrl: `${process.env.NEXT_PUBLIC_AUTH_URL}`,
+            })
+          }
+          className="btn btn-outline border border-gray-500 hover:bg-transparent disabled:bg-transparent ml-auto">
+          <ArrowRightOnRectangleIcon className="h-6 w-6" />
+        </Button>
+      </div>
     </Card.Body>
   </Card>
 );
