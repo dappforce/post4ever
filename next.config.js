ed889a04620a98be0e184402071e81a7248f556b
ical10 <mochhusnir@gmail.com>
2022-12-05 20:54:52 +0700

    feat: define cross-post page as homepage

--- next.config.js
+++ next.config.js
@@ -5,8 +5,17 @@ const nextConfig = {
   images: {
     domains: ["pbs.twimg.com", "abs.twimg.com"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/crosspost",
+        permanent: true,
+      },
+    ];
+  },
 };
 
-const withTM = require('next-transpile-modules')(['react-daisyui']);
+const withTM = require("next-transpile-modules")(["react-daisyui"]);
 
 module.exports = withTM(nextConfig);
