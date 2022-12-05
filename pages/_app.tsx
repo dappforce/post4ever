c77e9d73a64cb6e3e97c15b7a2e16399b647ba26
ical10 <mochhusnir@gmail.com>
2022-12-05 11:03:39 +0700

    chore: install and configure material-tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "@material-tailwind/react";
 
 function MyApp({ Component, pageProps }: AppProps<{ session: Session }>) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ThemeProvider>
+      <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ThemeProvider>
   );
 }
 
