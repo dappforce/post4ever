293bf3fc0e3658a2004f71f5def94c3b1f350dc3
ical10 <mochhusnir@gmail.com>
2022-12-07 02:08:28 +0700

    feat: handle checking owned spaces

--- src/components/SendTweetCard.tsx
+++ src/components/SendTweetCard.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, Card, Tooltip } from "react-daisyui";
 import { Select, Option } from "@material-tailwind/react";
 import Skeleton from "react-loading-skeleton";
+import { useSession } from "next-auth/react";
 import "react-loading-skeleton/dist/skeleton.css";
 import { useSubSocialApiHook } from "src/hooks/use-subsocial-api";
 import { useWalletStore } from "src/store";
@@ -9,15 +10,35 @@ import { TweetWithAuthorProps } from "src/types/common";
 
 type SendTweetCardProps = {
   disabled: boolean;
-  fetchedTweet: TweetWithAuthorProps;
+  fetchedTweet: TweetWithAuthorProps | null;
 };
 
 const SendTweetCard = ({ disabled, fetchedTweet }: SendTweetCardProps) => {
-  const { loadingSpaces, loadingCreatePost, spaces, createSpaceWithTweet, createPostWithSpaceId } =
-    useSubSocialApiHook();
+  const { data: session, status } = useSession();
+  const {
+    initApi,
+    loadingSpaces,
+    loadingCreatePost,
+    spaces,
+    createSpaceWithTweet,
+    createPostWithSpaceId,
+    checkSpaceOwnedBy,
+  } = useSubSocialApiHook();
   const { account } = useWalletStore(state => ({
     account: state.account,
   }));
+
+  useEffect(() => {
+    if (session) {
+      initApi({ mnemonic: session.mnemonic });
+    }
+  }, [session]);
+
+  useEffect(() => {
+    if (account) {
+      checkSpaceOwnedBy(account);
+    }
+  }, [account]);
 
   const [selectedSpaceId, setSelectedSpaceId] = useState<string | null>(null);
 
@@ -49,7 +70,7 @@ const SendTweetCard = ({ disabled, fetchedTweet }: SendTweetCardProps) => {
       bordered={false}
       className="shadow-md bg-white flex flex-col self-center items-center justify-center">
       <Card.Body className="gap-6">
-        <h2 className={`text-lg font-bold ${disabled ? "text-grey-600" : "text-neutral"}`}>
+        <h2 className={`text-lg font-bold ${disabled ? "text-[#A0ADB4]" : "text-neutral"}`}>
           3. Connect wallet and select a SS space
         </h2>
 
