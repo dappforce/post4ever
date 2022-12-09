2c5097bad1dd7e8cd010cc8b51b7a1deaaf9c882
ical10 <mochhusnir@gmail.com>
2022-12-09 20:06:45 +0700

    fix: create hover style for logout button

--- src/components/TwitterUserProfileCard.tsx
+++ src/components/TwitterUserProfileCard.tsx
@@ -60,8 +60,20 @@ const TwitterUserProfileCard = ({ disabled, authenticatedUser }: TwitterUserProf
                       callbackUrl: `${process.env.NEXT_PUBLIC_AUTH_URL}/crosspost`,
                     })
                   }
-                  className="btn btn-outline border border-gray-500 hover:bg-transparent disabled:bg-transparent ml-auto">
-                  <ArrowRightOnRectangleIcon className="h-6 w-6" />
+                  className="btn btn-outline border border-gray-500 hover:border-red-500 hover:bg-white hover:bg-opacity-0 hover:text-red-500 disabled:bg-transparent ml-auto">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6">
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+                    />
+                  </svg>
                 </Button>
               </div>
             </>
