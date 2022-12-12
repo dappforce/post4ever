7b5dd581acbcdbf60b1b3c66221237ac48c265f9
ical10 <mochhusnir@gmail.com>
2022-12-12 22:43:42 +0700

    fix: adjust skeleton style

--- src/components/SkeletonCard.tsx
+++ src/components/SkeletonCard.tsx
@@ -8,8 +8,8 @@ const SkeletonCard = () => {
     <Card
       key="skeleton-card"
       bordered={false}
-      className="rounded-[14px] shadow-md bg-white px-4 py-2 mt-4 h-fit border">
-      <Card.Body>
+      className="border rounded-lg border-[#d9d9d9] shadow-[0px_4px_+13px_#E1E6E8] bg-white h-fit">
+      <Card.Body className="card-body px-4 py-5 gap-[14px]">
         <div className="flex">
           <Skeleton inline circle width={40} height={40} containerClassName="w-50 mr-4" />
           <div className="flex-1">
