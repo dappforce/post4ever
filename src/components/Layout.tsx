ab07b80766c32cbd45d023047599641c1d9014e7
ical10 <mochhusnir@gmail.com>
2022-12-12 08:55:54 +0700

    chore: remove unused method

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -61,15 +61,6 @@ const Layout = ({ children }: LayoutProps) => {
     setIsOpen(false);
   };
 
-  const trimMiddleString = (text?: string, numberStringsKept = 5) => {
-    if (!text) return "";
-    const temp = `${text.slice(0, numberStringsKept)}...${text.slice(
-      text.length - numberStringsKept,
-    )}`;
-
-    return temp;
-  };
-
   return (
     <>
       <Sidebar
