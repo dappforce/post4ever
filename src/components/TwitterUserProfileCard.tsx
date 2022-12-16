9e6a0210d151dd2b8a0a41a5c7dc4ccb3dd68cad
ical10 <mochhusnir@gmail.com>
2022-12-16 16:58:38 +0700

    fix: change hover style and correct text

--- src/components/TwitterUserProfileCard.tsx
+++ src/components/TwitterUserProfileCard.tsx
@@ -12,6 +12,19 @@ type TwitterUserProfileCardProps = {
   authenticatedUser?: TweetUserProps;
 };
 
+const logoutIcon = (
+  <svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      className="group-hover:fill-red-500 group-hover:opacity-100"
+      id="Vector"
+      fill="black"
+      fillOpacity="0.54"
+      strokeWidth={0.5}
+      d="M10.332 2.66667L9.39203 3.60667L11.112 5.33333H4.33203V6.66667H11.112L9.39203 8.38667L10.332 9.33333L13.6654 6L10.332 2.66667ZM1.66536 1.33333H6.9987V0H1.66536C0.932031 0 0.332031 0.6 0.332031 1.33333V10.6667C0.332031 11.4 0.932031 12 1.66536 12H6.9987V10.6667H1.66536V1.33333Z"
+    />
+  </svg>
+);
+
 const TwitterUserProfileCard = ({ disabled, authenticatedUser }: TwitterUserProfileCardProps) => {
   const { data: session, status } = useSession();
 
@@ -59,44 +72,31 @@ const TwitterUserProfileCard = ({ disabled, authenticatedUser }: TwitterUserProf
               </div>
               <div className="flex justify-center items-center ml-auto">
                 <Button
+                  startIcon={logoutIcon}
                   shape="square"
-                  size="md"
+                  className="w-fit group btn btn-outline border border-gray-500/[0.54] px-4 normal-case text-gray-500 hover:border-red-500/[0.54] hover:bg-white hover:text-red-500/[0.54] disabled:bg-transparent ml-auto"
                   onClick={() =>
                     signOut({
                       callbackUrl: `${process.env.NEXT_PUBLIC_AUTH_URL}/crosspost`,
                     })
-                  }
-                  className="group btn btn-outline border border-gray-500 hover:border-red-500 hover:bg-white disabled:bg-transparent ml-auto">
-                  <svg
-                    width="14"
-                    height="12"
-                    viewBox="0 0 14 12"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      className="group-hover:fill-red-500 group-hover:opacity-100"
-                      id="Vector"
-                      fill="black"
-                      fillOpacity="0.54"
-                      strokeWidth={0.5}
-                      d="M10.332 2.66667L9.39203 3.60667L11.112 5.33333H4.33203V6.66667H11.112L9.39203 8.38667L10.332 9.33333L13.6654 6L10.332 2.66667ZM1.66536 1.33333H6.9987V0H1.66536C0.932031 0 0.332031 0.6 0.332031 1.33333V10.6667C0.332031 11.4 0.932031 12 1.66536 12H6.9987V10.6667H1.66536V1.33333Z"
-                    />
-                  </svg>
+                  }>
+                  <p className="hidden md:block">Disconnect</p>
                 </Button>
               </div>
             </>
           ) : (
             <Button
               className={`normal-case border-0 ${
-                !disabled ? "btn bg-gradient-to-r from-primary to-secondary" : "btn btn-disabled"
+                !disabled
+                  ? "btn bg-gradient-to-r from-primary to-secondary hover:brightness-90"
+                  : "btn btn-disabled"
               }`}
-              size="md"
               onClick={() =>
                 signIn("twitter", {
                   callbackUrl: `${process.env.NEXT_PUBLIC_AUTH_URL}/crosspost`,
                 })
               }>
-              Connect your Twitter account
+              Connect Twitter
             </Button>
           )}
         </div>
