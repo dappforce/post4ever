2abd10822bb93a09413e1d03abd74779f45d96ef
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-02-02 20:23:28 +0700

    Make hero section full screen

--- src/components/landing/HeroSection.tsx
+++ src/components/landing/HeroSection.tsx
@@ -4,14 +4,17 @@ import Diamond from "src/assets/diamond.png";
 import Button from "components/Button";
 import { useBreakpointThreshold } from "src/hooks/use-breakpoint";
 
+const NAVBAR_HEIGHT_OFFSET = 72;
 const HeroSection = () => {
   const mdUp = useBreakpointThreshold("md");
   return (
     <div
+      className="flex items-center"
       style={{
         background: "linear-gradient(106.83deg, #ff3ec9 0.85%, #5239ce 103.97%)",
+        height: `calc(100vh - ${NAVBAR_HEIGHT_OFFSET}px)`,
       }}>
-      <Container className="flex flex-col-reverse items-center gap-8 py-16 md:grid md:grid-cols-[1.25fr,1fr]">
+      <Container className="flex flex-col-reverse items-center gap-8 py-12 md:grid md:grid-cols-[1.25fr,1fr]">
         <div className="flex flex-col items-center justify-center md:items-start">
           <h1 className="mb-6 text-center font-unbounded text-2xl font-medium !leading-snug text-white md:mb-8 md:text-left md:text-3xl lg:text-4xl">
             Back up your tweets to Subsocial’s censorship resistant network
