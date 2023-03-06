bc7bc54e87be1027845d7e6036bb2d33845086cb
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-03-06 21:48:59 +0700

    Remove unused functions

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -7,28 +7,6 @@ export const trimMiddleString = (text?: string, numberStringsKept = 5) => {
   return result;
 };
 
-export const parseHashtag = (text: string) => {
-  const result = text.replace(
-    /(\s#)(\w+[a-zA-Z0-9]+)/g,
-    " [#$2](https://twitter.com/hashtag/$2?src=hashtag_click)",
-  );
-
-  return result;
-};
-
-export const parseUsername = (text: string) => {
-  const result = text.replace(/(?<!\w)@([a-zA-Z0-9_]+){1,15}/g, "[@$1](https://twitter.com/$1)");
-
-  return result;
-};
-
-export const parseTextToMarkdown = (text: string) => {
-  const hashtagParsed = parseHashtag(text);
-  const markdownWithLinks = parseUsername(hashtagParsed);
-
-  return markdownWithLinks;
-};
-
 export const urlMatcher = (text: string, urlToMatch: string) => {
   let isMatch = false;
 
