30dc98d890c53753b9be62b14bf4227e3caa0951
teodorus-nathaniel <nathankurnia26@gmail.com>
2023-03-06 21:10:21 +0700

    Change only parse twitter

--- src/components/render/TweetBody.tsx
+++ src/components/render/TweetBody.tsx
@@ -1,20 +1,30 @@
 import React, { useMemo } from "react";
-// import ReactMarkdown from "react-markdown";
-// import remarkGfm from "remark-gfm";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
-import { parseTextToMarkdown } from "src/utils/string";
+import { parseTwitterTextToMarkdown } from "@subsocial/utils";
 
 type TweetBodyProps = {
   text: string;
 };
 
 const TweetBody = ({ text }: TweetBodyProps) => {
-  // const parsedMD = useMemo(() => {
-  //   const result = parseTextToMarkdown(text);
-  //   return result;
-  // }, [text]);
+  const parsedMD = useMemo(() => {
+    const result = parseTwitterTextToMarkdown(text);
+    return result;
+  }, [text]);
 
-  return <span>asdfasdfads,{JSON.stringify(text)}</span>;
+  return (
+    <ReactMarkdown
+      linkTarget="_blank"
+      className="whitespace-pre-line"
+      components={{
+        a: ({ node, ...props }) => <a className="text-link-blue" {...props} />,
+      }}
+      remarkPlugins={[remarkGfm]}>
+      {parsedMD}
+    </ReactMarkdown>
+  );
 };
 
 export default TweetBody;
